1cd65210f8828cbeb4f4baa2ef07bedbed3cb358
mnandarmay <nandarm@sfu.ca>
2021-04-18 14:44:46 -0700

    test.R

---
+++ test.R
@@ -0,0 +1,16 @@
+# test 1
+library(ISLR)
+data(Wage)
+mc <- mars.control(Mmax=10)
+mout <- mars(wage ~ age + education, data=Wage, control=mc)
+ff <- fitted(mout)
+p1 <- predict(mout)
+p2 <- predict(mout,newdata=data.frame(age=Wage$age,education=Wage$education))
+head(cbind(ff,p1,p2)) # columns should be identical
+mout # tests print method
+summary(mout) #test summary method
+anova(mout) # test anova method
+plot(mout) # test plot method
+
+# test 2
+
